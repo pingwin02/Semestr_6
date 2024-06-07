d3fe3a3d6350e289e21afb2dcf83980e34175b4a
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2024-06-08 01:27:27 +0200

    Add KSR. 14 lab

---
+++ KSR/Lab14/LibrarySystem/Library.WebApi/Configuratrion/RabbitMQConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace Library.WebApi.Configuratrion
+{
+    public class RabbitMqConfiguration
+    {
+        public string ServerAddress { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
